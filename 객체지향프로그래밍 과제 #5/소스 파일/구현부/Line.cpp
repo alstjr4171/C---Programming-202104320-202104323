0d2b65a64cd1a6f8dc32189b48483d1e3a7d4f7e
alstjr4171 <80445950+alstjr4171@users.noreply.github.com>
2024-11-16 19:30:38 +0900

    Delete 객체지향프로그래밍 과제 #5 directory

--- 객체지향프로그래밍 과제 #5/소스 파일/구현부/Line.cpp
+++
@@ -1,8 +0,0 @@
-#include "Line.h"  // Line 클래스의 정의를 포함하는 헤더 파일을 포함
-#include <iostream>
-using namespace std;
-
-// Line 클래스에서 상속받은 draw() 메서드 Line 객체가 그려질 때 호출
-void Line::draw() {
-    cout << "Line" << endl;  // Line을 출력하여 선을 나타냄
-}
